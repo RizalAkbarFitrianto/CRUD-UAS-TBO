16e4b7c1a8c64abbdc9f2c81333f7a8c05ca5c77
RizalAkbarFitrianto <rizal.akbar.fitrianto@gmail.com>
2022-06-22 20:36:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Repository ini dibuat untuk memenuhi tugas Ujian Akhir Semester mata kuliah Teori Bahasa Formal dan Otomata.
+
+Nama  : Rizal Akbar Fitrianto
+NIM   : 23420001
+Prodi : Teknik Informatika 2020 B (Malam)
+
+
+
+# CRUD Data Karyawan PT GTS 
+CRUD dengan Template Admin LTE menggunakan Codeigniter
+
+Fitur : 
+  1. CRUD with Ajax
+  2. Datatable
+  3. Export dan Import ke Excel
+  4. Chart
+  
+Akun Login :
+  1. username : anne / password : anne
+  2. username : auwfar/ password : auwfar
---
+++ database/gts.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 22, 2022 at 02:23 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.4.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`, `foto`) VALUES
+(1, 'auwfar', 'f0a047143d1da15b630c73f0256d5db0', 'Achmad Chadil Auwfar', 'Koala.jpg'),
+(2, 'anne', 'e3fb62ebfa4f36acf5cbff6a6ed0f2e0', 'Anne', '256x256_color.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelamin`
+--
+
+CREATE TABLE `kelamin` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelamin`
+--
+
+INSERT INTO `kelamin` (`id`, `nama`) VALUES
+(1, 'Laki laki'),
+(2, 'Perempuan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kota`
+--
+
+CREATE TABLE `kota` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`id`, `nama`) VALUES
+(21, 'Surabaya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id` varchar(255) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL,
+  `telp` varchar(255) DEFAULT NULL,
+  `id_kota` int(11) DEFAULT NULL,
+  `id_kelamin` int(1) DEFAULT NULL,
+  `id_posisi` int(11) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id`, `nama`, `telp`, `id_kota`, `id_kelamin`, `id_posisi`, `status`) VALUES
+('10', 'Wardha Ayu Ambarwati', '082199568540', 21, 2, 3, 1),
+('7', 'Achmad Chadil Auwfar', '08984119934', 2, 1, 1, 1),
+('8a557f1dbfa9513e8a40fc856018a7f9', 'Fris Dyana', '085632147568', 21, 2, 2, 1),
+('d0a0cbd58ff4c1102f3063c31f9729bf', 'Tamara Tsania Yahya', '082337055508', 21, 2, 3, 1),
+('5134a875c6e8a6576b0a9397d15146c2', 'Rizal Akbar Fitrianto', '082232782088', 21, 1, 5, 1),
+('b9390081ddbef15c241ff2e48bd3c9bc', 'Agestita Meica', '081236548567', 21, 2, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posisi`
+--
+
+CREATE TABLE `posisi` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posisi`
+--
+
+INSERT INTO `posisi` (`id`, `nama`) VALUES
+(2, 'HRD'),
+(3, 'Keuangan'),
+(5, 'Web Developer');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kelamin`
+--
+ALTER TABLE `kelamin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kota`
+--
+ALTER TABLE `kota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posisi`
+--
+ALTER TABLE `posisi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `kota`
+--
+ALTER TABLE `kota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `posisi`
+--
+ALTER TABLE `posisi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
